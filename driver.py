8bd9c34a363e02f1a7612d90eaef40b800ebdf0f
Hurshid Karimov <Hurshid_Karimov@ultimatesoftware.com>
2018-02-13 22:53:23 -0500

    initial commit with v1

--- driver.py
+++ driver.py
@@ -5,7 +5,15 @@ from AppLibrary import *
 # This functions tests internet speed and reports the results in tweet if internet speed is lower than expected.
 # testSpeedAndReport()
 
+numberOfTests = askUserToProvideTotalNumberOfTestsToRun()
 maxDownloadSpeed = askUserToProvideMaxDownloadSpeedLimit()
 asl = askUserToProvideASL(maxDownloadSpeed)
-downloadSpeed = runSpeedTest()
-postATweet(downloadSpeed, maxDownloadSpeed)
+i = 0
+while i < numberOfTests:
+    print 'TEST #', i+1, 'STARTED'
+    downloadSpeed = str(runSpeedTest())
+    print 'Current download speed:', downloadSpeed
+    tweetId = postATweet(downloadSpeed, maxDownloadSpeed)
+    openLinkInBrowser(tweetId, downloadSpeed)
+    print 'TEST #', i+1, 'ENDED'
+    i += 1
